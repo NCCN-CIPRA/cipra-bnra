5e3a2ffdf2b11de152382ac2a873d06d5c39867a
NCCN-JoepDriesen <joep.driesen@nccn.fgov.be>
2022-09-28 20:00:35 +0200

    Registration flow fix

--- src/pages/main/AuthenticationPage.tsx
+++ src/pages/main/AuthenticationPage.tsx
@@ -57,6 +57,8 @@ export default function AuthenticationPage() {
   const [resetPassword, setResetPassword] = useState(false);
   const [registering, setRegistering] = useState(false);
 
+  const [registrationForm, setRegistrationForm] = useState("");
+
   const handleChangeTab = async (
     event: React.SyntheticEvent,
     newValue: number
@@ -125,7 +127,143 @@ export default function AuthenticationPage() {
       }),
     });
 
-    if (response.status === 200) {
+    if (response.status === 500) {
+      const responseText = await response.text();
+
+      const formHtml = `<form method="post" action="./Register?returnUrl=%2F&amp;invitationCode=q7tqXtl1mwxcZYMy0HlbEFepkVgUwCQvDelqmCRo32M0G3aJ3KPNpjHHmSncMqrAZQHuoT21cNorTyuC2HKYt65alF7-58DbVqp6P6MSRk18KfOPhCxnopWYgXlnxjkyEVN-x24fZow4iZojROlwqTNg2MBvwLBKHEel4RgZzAY-&amp;msCorrelationId=cbd333b9-d9ef-44b4-a166-e8137d22e432&amp;instanceId=d5e7f6ef215086c3f4017c096aaf3e8cd5bc855f64a16876f9f4b4f13b14531b&amp;tenantId=de192ca2-f778-4bb0-b5f1-5cce5803789a&amp;portalId=6355a931-0a0e-4a46-b516-8e55896cca49&amp;orgId=303cf766-f85b-4e52-8a06-e0a807a472a8&amp;environmentId=Default-de192ca2-f778-4bb0-b5f1-5cce5803789a&amp;portalApp=site-3944e3b0-61bd-4033-917b-e8e55214e662-EUn&amp;portalType=CDSStarterPortal&amp;portalProductionOrTrialType=Trial&amp;licenseType=PowerPortal&amp;portalVersion=9.4.8.62&amp;islandId=107&amp;portalDomain=https%3A%2F%2Fbnra.powerappsportals.com" id="Register">
+      <div class="aspNetHidden">
+      <input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="" />
+      <input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="" />
+      <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/Z05oLvlgACZmlHLTYlfU8j7z1PsgMaKqwx/CHMccb7aPuPIVPNmVvmIlJeMk8fGXN3Wpl0YSE5Xjykkt5Buv/NL2l/vtG3RQlkTTjX4jtfZUCoBp9E6FyxytQlX+Qw48GAZCT/1eyv+m4zHXUw9zddfadHBwH+QOi3H7GAiDwKXcX3kzUEJHirNQpAo5UrQdETxGH5siwIAl9DFe/rrzA0u/PQziPTsfTmrD429f1C6/vkI4zugX9x4VsOg+XdUf7pdDYT5rLQ81EfI4nGBPKi/r7k2YifXcchVbk+IpN8PO5jtVZHIY3WrQKZC1PENBG8zo7br55x6qkJvmr2+mw==" />
+      </div>
+      
+      <script type="text/javascript">
+      //<![CDATA[
+      var theForm = document.forms['Register'];
+      if (!theForm) {
+          theForm = document.Register;
+      }
+      function __doPostBack(eventTarget, eventArgument) {
+          if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+              theForm.__EVENTTARGET.value = eventTarget;
+              theForm.__EVENTARGUMENT.value = eventArgument;
+              theForm.submit();
+          }
+      }
+      //]]>
+      </script>
+      
+      
+      <script src="/WebResource.axd?d=pynGkmcFUV13He1Qd6_TZJPDfSZt44QEqKwGCW7u0JfFxENkRZy00TQmujRY054T3y5N9br5kRs6nZrqSUPsKQ2&amp;t=637814653746327080" type="text/javascript"></script>
+      
+      
+      <script src="/ScriptResource.axd?d=NJmAwtEo3Ipnlaxl6CMhvvfMW8AluJP1qOGG6RANIqj7qGHS0thdGVj1_WGrcI-DRb1hJmSYkkyrT1O6VReC96Y-0IveuPzi17dViuQGEqs7nGvtLMb1OjwQs-6P9OPmKe1bvf-SHGMAzg-gh4sPMECd8EQFs3UI6kiRO35VLJU1&amp;t=49337fe8" type="text/javascript"></script>
+      <script src="/ScriptResource.axd?d=dwY9oWetJoJoVpgL6Zq8OCk2NQxKtV_f0HwqeHAFTI1wfQkXnlQLJ5SXF31Iphi3dkQKTQRntOC5DQv3JELCSQHYDpfLlW57_Qnvl6CYwOATfh1oeoHj_w1uyeDIK-8seocfONSOTArV6AYeuo0qwBOkCndBL3b2KfBYbFbEeD81&amp;t=49337fe8" type="text/javascript"></script>
+      <div class="aspNetHidden">
+      
+        <input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="82F9452B" />
+        <input type="hidden" name="__VIEWSTATEENCRYPTED" id="__VIEWSTATEENCRYPTED" value="" />
+      </div>
+          <script type="text/javascript">
+      //<![CDATA[
+      Sys.WebForms.PageRequestManager._initialize('ctl00$ctl00$ContentContainer$MainContent$MainContent$ctl00', 'Register', [], [], [], 90, 'ctl00$ctl00');
+      //]]>
+      </script>
+      
+          <div id="ContentContainer_MainContent_MainContent_SecureRegister">
+        
+            
+              <div class="alert alert-info">
+                Inwisselcode:
+                <strong>q7tqXtl1mwxcZYMy0HlbEFepkVgUwCQvDelqmCRo32M0G3aJ3KPNpjHHmSncMqrAZQHuoT21cNorTyuC2HKYt65alF7-58DbVqp6P6MSRk18KfOPhCxnopWYgXlnxjkyEVN-x24fZow4iZojROlwqTNg2MBvwLBKHEel4RgZzAY-</strong>
+              </div>
+            
+            <div class="row">
+              <div id="ContentContainer_MainContent_MainContent_LocalLogin">
+          
+                <div class="col-md-6">
+                  <div class="form-horizontal">
+                        <h1 class="login-heading-section">
+                          <span id="ContentContainer_MainContent_MainContent_RegisterLocalFormHeading"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">Registreren voor een nieuw lokaal account</span></span></span>
+                        </h1>
+                        
+                        <div id="ContentContainer_MainContent_MainContent_ShowEmail">
+            
+                          <div class="form-group">
+                            <label class="col-sm-4 control-label required" for="EmailTextBox">
+                              <span id="ContentContainer_MainContent_MainContent_EmailLabel"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">E-mail</span></span></span></label>
+                            <div class="col-sm-8">
+                              <input name="ctl00$ctl00$ContentContainer$MainContent$MainContent$EmailTextBox" type="text" value="a@a.com" id="EmailTextBox" class="form-control" />
+                            </div>
+                          </div>
+                        
+          </div>
+                        <div id="ContentContainer_MainContent_MainContent_ShowUserName">
+            
+                          <div class="form-group">
+                            <label class="col-sm-4 control-label required" for="UserNameTextBox">
+                              <span id="ContentContainer_MainContent_MainContent_UsernameLabel"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">Gebruikersnaam</span></span></span></label>
+                            <div class="col-sm-8">
+                              <input name="ctl00$ctl00$ContentContainer$MainContent$MainContent$UserNameTextBox" type="text" id="UserNameTextBox" class="form-control" />
+                            </div>
+                          </div>
+                        
+          </div>
+                        <div class="form-group">
+                          <label class="col-sm-4 control-label required" for="PasswordTextBox">
+                            <span id="ContentContainer_MainContent_MainContent_PasswordLabel"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">Wachtwoord</span></span></span></label>
+                          <div class="col-sm-8">
+                            <input name="ctl00$ctl00$ContentContainer$MainContent$MainContent$PasswordTextBox" type="password" id="PasswordTextBox" class="form-control" autocomplete="new-password" />
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <label class="col-sm-4 control-label required" for="ConfirmPasswordTextBox">
+                            <span id="ContentContainer_MainContent_MainContent_ConfirmPasswordLabel"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">Wachtwoord bevestigen</span></span></span></label>
+                          <div class="col-sm-8">
+                            <input name="ctl00$ctl00$ContentContainer$MainContent$MainContent$ConfirmPasswordTextBox" type="password" autocomplete="off" id="ConfirmPasswordTextBox" class="form-control" />
+                          </div>
+                        </div>
+                        
+                        <div class="form-group">
+                          <div class="col-sm-offset-4 col-sm-8">
+                            <input type="submit" name="ctl00$ctl00$ContentContainer$MainContent$MainContent$SubmitButton" value="Registreren" id="SubmitButton" title="Registreren" class="btn&#32;btn-primary" />
+                          </div>
+                        </div>
+                    </div>
+                </div>
+              
+        </div>
+              <div id="ContentContainer_MainContent_MainContent_ExternalLogin">
+          
+                <div class="col-md-6">
+                  <input name="__RequestVerificationToken" type="hidden" value="9KdzVG8gCI3jPQFKyKbLr1oeXFcHDu2v63NKpf-SXUlwaMtOORoTFCFOTR3zWm8jGS0Z3Ah3D6ZHgfP15XeU4IvMVKFTbv1N81eKp4HRQEI1" />
+                  <div class="form-horizontal">
+                      <h1 class="login-heading-section">
+                        <span id="ContentContainer_MainContent_MainContent_RegisterExternalLabel"><span class="xrm-editable-text&#32;xrm-attribute"><span class="xrm-attribute-value">Registreren met een extern account</span></span></span></h1>
+                      <div id="ContentContainer_MainContent_MainContent_ExternalLoginButtons">
+            <button onclick="__doPostBack('ctl00$ctl00$ContentContainer$MainContent$MainContent$ctl01','')" name="provider" type="submit" class="btn&#32;btn-primary&#32;btn-line" title="Azure&#32;AD" value="https://login.windows.net/de192ca2-f778-4bb0-b5f1-5cce5803789a/">Azure AD</button>&nbsp;
+          </div>
+                  </div>
+                </div>
+              
+        </div>
+            </div>
+          
+      </div>
+          
+          <script type="text/javascript">
+            $(document).ready(function () {
+              document.querySelector('title').innerHTML = ($('.nav-tabs > li.active').text() + "&nbsp;· Startportal");
+              $("#SubmitButton").on("click", function () {
+                $.blockUI({ message: null, overlayCSS: { opacity: .3 } });
+              });
+              portal.UpdateValidationSummary("ValidationSummary1");
+            });
+              </script>
+        </form>`;
+
+      setRegistrationForm(formHtml);
+
       setRegistering(true);
     } else {
       // TODO: Error
@@ -133,42 +271,17 @@ export default function AuthenticationPage() {
   };
 
   const handleRegister = async () => {
-    const response = await fetch(
-      `https://bnra.powerappsportals.com/Register?invitationCode=${inviteCode}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          __RequestVerificationToken:
-            localStorage.getItem("antiforgerytoken") || "",
-          ctl00$ctl00$ContentContainer$MainContent$MainContent$EmailTextBox:
-            email,
-          ctl00$ctl00$ContentContainer$MainContent$MainContent$UserNameTextBox:
-            email,
+    document.getElementById("EmailTextBox")!.setAttribute("value", email);
+    document.getElementById("UserNameTextBox")!.setAttribute("value", email);
+    document.getElementById("PasswordTextBox")!.setAttribute("value", password);
+    document
+      .getElementById("ConfirmPasswordTextBox")!
+      .setAttribute("value", password2);
 
-          ctl00$ctl00$ContentContainer$MainContent$MainContent$PasswordTextBox:
-            password,
-          ctl00$ctl00$ContentContainer$MainContent$MainContent$ConfirmPasswordTextBox:
-            password,
-          ctl00$ctl00$ContentContainer$MainContent$MainContent$SubmitButton:
-            "Registreren",
-          __EVENTTARGET: "",
-          __EVENTARGUMENT: "",
-          __VIEWSTATE:
-            "FswsVIzb2EVuOq0qw7rclKoQXvY3hQGMrk6mymoI4oWIrk5gUdw5DGMdmzWhcVymHk%2FV5GQ10OUppZEWcYTcVWbTNsYWJEf8x%2F7hPYz1KyArPqJup%2BYMyjtnGwATUC7cmHprMX5ifrzkkMJ5xMHQxecbMD6fHCNsJ6MnofIcnmC%2F2pqVn3ZwiyuyNrTdZm3PVjbk6Si1KOy8zoxAF91gsbTENQ3AyZLs4973wgm48FAi9unYl0Nx30DePobURd2HyipCDYaFcJ4%2BGN6C%2F0y1UJhkvgj9p4IlfBYg%2FWol1sSkX95M2ck6UAj0nW8Xjk44DnCECFo0dClUyeNlOzu6kA%3D%3D",
-          __VIEWSTATEGENERATOR: "82F9452B",
-          __VIEWSTATEENCRYPTED: "",
-        }),
-      }
-    );
-
-    if (response.status === 200) {
-      navigate("/");
-    } else {
-      // TODO: Error
-    }
+    document
+      .getElementById("registration-form")!
+      .getElementsByTagName("form")[0]
+      .submit();
   };
 
   return (
@@ -340,6 +453,11 @@ export default function AuthenticationPage() {
                     Register
                   </Button>
                 </Stack>
+                <div
+                  id="registration-form"
+                  style={{ display: "none" }}
+                  dangerouslySetInnerHTML={{ __html: registrationForm }}
+                />
               </>
             ) : (
               <>
