8ee45f3e07ff8fc745e219b8dcca6dc139264111
NCCN-JoepDriesen <joep.driesen@nccn.fgov.be>
2022-09-28 16:32:19 +0200

    Added anti forgery token

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import AuthenticationPage from "./pages/main/AuthenticationPage";
@@ -6,6 +7,24 @@ import ValidationIntroPage from "./pages/validation/ValidationIntroPage";
 import ValidationPage from "./pages/validation/ValidationPage";
 
 function App() {
+  useEffect(() => {
+    const getAntiForgeryToken = async () => {
+      const response = await fetch(
+        `https://bnra.powerappsportals.com/_layout/tokenhtml?_=${Date.now()}`,
+        {
+          method: "GET",
+        }
+      );
+
+      localStorage.setItem(
+        "antiforgerytoken",
+        await (await response.text()).split("value")[1].split('"')[1]
+      );
+    };
+
+    getAntiForgeryToken();
+  }, []);
+
   const router = createHashRouter([
     {
       path: "/",
--- src/pages/main/AuthenticationPage.tsx
+++ src/pages/main/AuthenticationPage.tsx
@@ -8,6 +8,7 @@ import {
   Checkbox,
   Stack,
   Button,
+  CssBaseline,
 } from "@mui/material";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
@@ -46,6 +47,8 @@ export default function AuthenticationPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
+  const [inviteCode, setInviteCode] = useState("");
+  const [resetPassword, setResetPassword] = useState(false);
 
   const handleChangeTab = async (
     event: React.SyntheticEvent,
@@ -60,20 +63,48 @@ export default function AuthenticationPage() {
         },
         body: new URLSearchParams({
           __RequestVerificationToken:
-            document.cookie
-              .split("; ")
-              .find((row) => row.startsWith("__RequestVerificationToken="))
-              ?.split("=")[1] || "",
+            localStorage.getItem("antiforgerytoken") || "",
           provider:
             "https://login.windows.net/de192ca2-f778-4bb0-b5f1-5cce5803789a/",
         }),
       });
     } else {
+      setResetPassword(false);
       setTab(newValue);
     }
   };
 
   const handleLogin = async () => {
+    fetch("https://bnra.powerappsportals.com/SignIn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        __RequestVerificationToken:
+          localStorage.getItem("antiforgerytoken") || "",
+        Username: email,
+        PasswordValue: password,
+        RememberMe: String(remember),
+      }),
+    });
+  };
+
+  const handleForgotPassword = async () => {
+    fetch("https://bnra.powerappsportals.com/ForgotPassword", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        __RequestVerificationToken:
+          localStorage.getItem("antiforgerytoken") || "",
+        Email: email,
+      }),
+    });
+  };
+
+  const handleRegister = async () => {
     fetch("https://bnra.powerappsportals.com/SignIn", {
       method: "POST",
       headers: {
@@ -92,86 +123,129 @@ export default function AuthenticationPage() {
     });
   };
 
-  const handleForgotPassword = async () => {
-    fetch("https://bnra.powerappsportals.com/ForgotPassword", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        __RequestVerificationToken:
-          document.cookie
-            .split("; ")
-            .find((row) => row.startsWith("__RequestVerificationToken="))
-            ?.split("=")[1] || "",
-        Email: email,
-      }),
-    });
-  };
-
   return (
-    <Container maxWidth="md" component={Paper} sx={{ mt: 8 }}>
-      <Box sx={{ width: "100%" }}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs
-            value={tab}
-            onChange={handleChangeTab}
-            aria-label="basic tabs example"
-          >
-            <Tab label="Log In (External Expert)" {...a11yProps(0)} />
-            <Tab label="Log In (Internal NCCN)" {...a11yProps(0)} />
-            <Tab label="Register" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        <TabPanel value={tab} index={0}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <TextField
-              id="outlined-email-input"
-              label="Email"
-              type="email"
-              autoComplete="current-email"
-              fullWidth
-              sx={{ my: 2 }}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              fullWidth
-              sx={{ my: 2 }}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <FormControlLabel
-              control={<Checkbox name="remember" />}
-              label="Remember me"
-              sx={{ ml: 0 }}
-              checked={remember}
-              onChange={(e) => setRemember(!remember)}
-            />
+    <>
+      <CssBaseline />
+      <Container maxWidth="md" component={Paper} sx={{ mt: 8 }}>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={tab}
+              onChange={handleChangeTab}
+              aria-label="basic tabs example"
+            >
+              <Tab label="Log In (External Expert)" {...a11yProps(0)} />
+              <Tab label="Log In (Internal NCCN)" {...a11yProps(0)} />
+              <Tab label="Register" {...a11yProps(1)} />
+            </Tabs>
           </Box>
-          <Stack spacing={2} direction="row" mt={4}>
-            <Button variant="contained" onClick={handleLogin}>
-              Login
-            </Button>
-            <Button variant="outlined">Forgot Password</Button>
-          </Stack>
-        </TabPanel>
-        <TabPanel value={tab} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={tab} index={2}>
-          Item Three
-        </TabPanel>
-      </Box>
-    </Container>
+          <TabPanel value={tab} index={0}>
+            {resetPassword ? (
+              <>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <TextField
+                    id="outlined-email-input"
+                    label="Email"
+                    type="email"
+                    autoComplete="current-email"
+                    fullWidth
+                    sx={{ my: 2 }}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Box>
+                <Stack spacing={2} direction="row" mt={4}>
+                  <Button variant="contained" onClick={handleRegister}>
+                    Request Password Reset
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => setResetPassword(false)}
+                  >
+                    Log In
+                  </Button>
+                </Stack>
+              </>
+            ) : (
+              <>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <TextField
+                    id="outlined-email-input"
+                    label="Email"
+                    type="email"
+                    autoComplete="current-email"
+                    fullWidth
+                    sx={{ my: 2 }}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    fullWidth
+                    sx={{ my: 2 }}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <FormControlLabel
+                    control={<Checkbox name="remember" />}
+                    label="Remember me"
+                    sx={{ ml: 0 }}
+                    checked={remember}
+                    onChange={(e) => setRemember(!remember)}
+                  />
+                </Box>
+                <Stack spacing={2} direction="row" mt={4}>
+                  <Button variant="contained" onClick={handleLogin}>
+                    Login
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    onClick={() => setResetPassword(true)}
+                  >
+                    Forgot Password
+                  </Button>
+                </Stack>
+              </>
+            )}
+          </TabPanel>
+          <TabPanel value={tab} index={2}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <TextField
+                id="outlined-email-input"
+                label="Invitation Code"
+                type="invitationCode"
+                fullWidth
+                sx={{ my: 2 }}
+                value={inviteCode}
+                onChange={(e) => setInviteCode(e.target.value)}
+              />
+            </Box>
+            <Stack spacing={2} direction="row" mt={4}>
+              <Button variant="contained" onClick={handleRegister}>
+                Register
+              </Button>
+            </Stack>
+          </TabPanel>
+        </Box>
+      </Container>
+    </>
   );
 }
