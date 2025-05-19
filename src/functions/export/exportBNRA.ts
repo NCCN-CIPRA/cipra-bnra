cd34d7e7ceafbe5951ebc3db7de8c78f0541e43b
Joep Driesen <joep.driesen@nccn.fgov.be>
2025-05-19 13:00:21 +0200

    Worker fix?

--- src/functions/export/exportBNRA.ts
+++ src/functions/export/exportBNRA.ts
@@ -16,7 +16,7 @@ export function getExporter() {
   }
 
   return wrap(
-    new Worker(new URL("./export.worker.js"))
+    new Worker(new URL("./export.worker.js", import.meta.url))
   ) as unknown as typeof import("./export.worker");
 }
 
