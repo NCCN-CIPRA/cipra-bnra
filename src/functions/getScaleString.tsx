d025ad6e7170675867076d365f6b316762c13915
Joep Driesen <joep.driesen@nccn.fgov.be>
2026-05-12 12:16:56 +0200

    Fixed scale strings

--- src/functions/getScaleString.tsx
+++ src/functions/getScaleString.tsx
@@ -1,32 +1,25 @@
 export function getProbabilityScaleString(value: number, maxScale: number = 5) {
-  const p = value / maxScale;
+  const p = 7 * (value / maxScale);
 
-  if (p < 0.07) return "Impossible";
-  if (p < 0.14) return "Theoretical";
-  if (p < 0.21) return "Negligible";
-  if (p < 0.29) return "Remote";
-  if (p < 0.36) return "Rare";
-  if (p < 0.43) return "Improbable";
-  if (p < 0.5) return "Unlikely";
-  if (p < 0.57) return "Marginal";
-  if (p < 0.64) return "Possible";
-  if (p < 0.71) return "Probable";
-  if (p < 0.79) return "Likely";
-  if (p < 0.86) return "Expected";
-  if (p < 0.93) return "Imminent";
-  if (p < 1.0) return "Inevitable";
-  return "Certain";
+  if (p < 0.1) return "Impossible";
+  if (p < 1) return "Very Low";
+  if (p < 2) return "Low";
+  if (p < 3) return "Low - Medium";
+  if (p < 4) return "Medium";
+  if (p < 5) return "Medium - High";
+  if (p < 6) return "High";
+  return "Very High";
 }
 
 export function getImpactScaleString(value: number, maxScale: number = 5) {
-  const p = value / maxScale;
+  const p = 7 * (value / maxScale);
 
-  if (p < 0.07) return "None"; //
-  if (p < 0.21) return "Negligible"; // 1
-  if (p < 0.36) return "Minimal"; // 2
-  if (p < 0.5) return "Moderate"; // 3
-  if (p < 0.64) return "Significant"; // 4
-  if (p < 0.79) return "Serious"; // 5
-  if (p < 0.93) return "Severe"; // 6
-  return "Catastrophic"; // 7
+  if (p < 0.1) return "No Impact"; //
+  if (p < 1) return "Very Low";
+  if (p < 2) return "Low";
+  if (p < 3) return "Low - Medium";
+  if (p < 4) return "Medium";
+  if (p < 5) return "Medium - High";
+  if (p < 6) return "High";
+  return "Very High";
 }
